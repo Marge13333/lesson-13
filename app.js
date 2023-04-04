c4e2455bbefa1884210d87cd560a683432d7c828
Marge13333 <113912686+Marge13333@users.noreply.github.com>
2023-04-04 17:23:55 +0400

    create js

---
+++ app.js
@@ -0,0 +1,28 @@
+/////user info
+let userName = 'irakli';
+let userAge = 15;
+let isStutent = true;
+let isEmployed = false;
+let myUndefined;
+let myNull = null;
+ 
+
+/////My PC
+const myComputerManufacturer = "DESKTOP-FO0R56H";
+const myComputerMemory = 8.00;
+const myComputerOS = 19044.2728;
+const computerBuyingYear = 2018;
+const currentYear = 2023;
+
+
+const myComputerIs = "my computer is " + myComputerManufacturer;
+console.log(myComputerIs)
+
+const itRunsOn = "it runs on " + myComputerMemory + 'GB';
+console.log(itRunsOn)
+
+const andIveBeenUsingItFor = "and I've been using it for " + (currentYear - computerBuyingYear) + " years";
+console.log(andIveBeenUsingItFor)
+
+
+////////////javascrips vgrdznob :DDDDD
